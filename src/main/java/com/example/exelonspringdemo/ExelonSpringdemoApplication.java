2fe2ca5969733aafcba71147e6f98a49ebe932bd
quivsoth <phillip.knezevich@gmail.com>
2021-02-15 22:52:14 -0500

    added maven plugin

---
+++ src/main/java/com/example/exelonspringdemo/ExelonSpringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.exelonspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExelonSpringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExelonSpringdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exelonspringdemo/ExelonSpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exelonspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExelonSpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
